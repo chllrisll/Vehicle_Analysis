28e8f9f954e240ab8dd587208270da0f22e6965b
Chris Lee <49954261+chllrisll@users.noreply.github.com>
2022-02-08 00:28:57 -0600

    R Challenge

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -17,4 +17,31 @@ lot_summary <- mecha_table %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
 
 #Deliv 3
 #determine if the PSI across all manufacturing lots is statistically different from the population mean of 1,500 pounds per square inch.
-t.test(mecha_table$PSI, mu =1500) 
+t.test(mecha_table$PSI, mu=1500) 
+#One Sample t-test
+
+#data:  mecha_table$PSI
+#t = -1.8931, df = 149, p-value = 0.06028
+#alternative hypothesis: true mean is not equal to 1500
+#95 percent confidence interval:
+#  1497.507 1500.053
+#sample estimates:
+#  mean of x 
+#1498.78 
+
+#box plot: PSI All Lots
+plt1 <- ggplot(mecha_table,aes(y=PSI)) #import dataset into ggplot2
+plt1 + geom_boxplot() #add boxplot
+
+#box plot: PSI of each Lot
+plt2 <- ggplot(mecha_table,aes(x=Manufacturing_Lot,y=PSI)) #import dataset into ggplot2
+plt2 + geom_boxplot()
+
+#Deliv 4
+Lot1 <- subset(mecha_table, Manufacturing_Lot=="Lot1")
+Lot2 <- subset(mecha_table, Manufacturing_Lot=="Lot2")
+Lot3 <- subset(mecha_table, Manufacturing_Lot=="Lot3")
+
+t.test(Lot1$PSI, mu=1500)
+t.test(Lot2$PSI, mu=1500)
+t.test(Lot3$PSI, mu=1500)
